9b0985e8ed832f9193370e0bdeabecd9f435ba5a
uditahuja <udkapoor82@gmail.com>
2020-10-14 09:00:59 +0530

    done

--- d.java
+++ d.java
@@ -3,5 +3,8 @@ class d
   public static void main(String args[])
   {
     System.out.println("Hello");
+    System.out.println("Hello");
+    System.out.println("Hello");
+    System.out.println("Hello");
   }
 }
